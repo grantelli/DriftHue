3a85766cdc755b22fdd7dd86933776bff14d64d6
Grant Elliott <grant1275@gmail.com>
2024-10-29 18:38:24 -0500

    Add location/destination question page

--- routespire/src/App.tsx
+++ routespire/src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Questionnaire from './components/Questionnaire/Questionnaire';
+import Questionnaire from './components/Questions/Questions';
 
 function App() {
   return (
